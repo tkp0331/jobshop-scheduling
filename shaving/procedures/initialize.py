1ad54d96621f2a65bacd7ba99c121541795d199e
鈴木貴大 <merioda.seven.24@gmail.com>
2021-10-13 05:59:20 +0900

    bug fix of initialize

--- shaving/procedures/initialize.py
+++ shaving/procedures/initialize.py
@@ -4,7 +4,7 @@ from ..tree.balanced_tree import BalancedJobTree
 
 
 def initialize_sigma_and_xi(job_tree: BalancedJobTree) -> None:
-    """Calculate initial value of each job's sigma and xi in balanced binary tree.
+    """Calculate initial value of each job's sigma and xi in balanced binary tree. 
 
     Args:
         job_tree (BalancedJobTree):
@@ -18,8 +18,13 @@ def initialize_sigma_and_xi(job_tree: BalancedJobTree) -> None:
 
     # initialization starts from leaves
     job_nodes = job_tree.leaves.copy()
+    initialized_nodes = set()
     while True:
         job_node = job_nodes.popleft()
+        if isinstance(job_node, NullJobNode):
+            # This block is executed when root node is initialized right before.
+            # This algorithm proceeds from leaves to root, so finish initialization.
+            return
 
         parent_job_node = job_node.parent
         lchild_job_node = job_node.left
@@ -33,13 +38,9 @@ def initialize_sigma_and_xi(job_tree: BalancedJobTree) -> None:
             job_node.q + job_node.sigma,
             rchild_job_node.xi
         )
-
-        if len(job_nodes) == 0:
-            # This block is executed when root node is initialized right before.
-            # This algorithm proceeds from leaves to root, so finish initialization.
-            return
+        initialized_nodes = {job_node} | initialized_nodes
 
         # NOTE: the data structure of job_nodes is deque, not set.
         # We need to take measures to initialize twice accidentally.
-        if parent_job_node is not job_nodes[-1]:
+        if parent_job_node not in initialized_nodes:
             job_nodes.append(parent_job_node)
